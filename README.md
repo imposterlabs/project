4d34745373a66255d672bd3d277670d2d8239b89
Yash Kumar Verma <yk.verma2000@gmail.com>
2022-01-08 06:29:57 +0530

    feat: sample axios request from MayaTrigger

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Maya MVP
+
+
+## Testing
+It is recommended to use a service like [webhook.site](https://webhook.site/) to inspect the requests.
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,27 @@
-import app from "./app"
+import axios from "axios"
+import { MayaTriggerDefinition } from "./parser/core/interface"
+import { METHOD } from "./parser/core/method/index"
+import { Context } from "./parser/core/context/interface"
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
+
+const triggerDefinition: MayaTriggerDefinition = {
+    url: "https://webhook.site/b0ae265d-839f-45fa-9774-c2d8bed11ddb",
+    body: { something: "awesome", people: [1, 2, 3, 4] },
+    method: METHOD.GET
+}
+
+const context: Context = {
+    environment: {},
+    prompt: {}
+}
+
+axios.request({
+    url: typeof triggerDefinition.url === "string" ? triggerDefinition.url : triggerDefinition.url(context),
+    method: triggerDefinition.method.axiosProvider,
+    data: triggerDefinition.body,
+}).then(res => {
+    console.log(res.data)
 })
+    .catch(err => {
+        console.log(err)
+    })
